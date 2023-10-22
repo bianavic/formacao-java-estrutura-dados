8943a8b28c4718e0847e84e2d62ac87d76245e0e
bianavic <bianavic@gmail.com>
2023-10-22 09:35:50 -0300

    implementado metodo interno get, e metodo para validar tamanho indice

--- src/main/java/org/edu/fabs/lista/encadeada/ListaEncadeada.java
+++ src/main/java/org/edu/fabs/lista/encadeada/ListaEncadeada.java
@@ -17,10 +17,39 @@ public class ListaEncadeada<T> {
         }
 
         No<T> noAuxiliar = referenciaEntrada; // recebera aquel referencia de entrada
-        for (int i = 0; i < this.size() -1; i++) {
+        for (int i = 0; i < this.size() - 1; i++) {
             noAuxiliar = noAuxiliar.getProximoNo();
         }
         noAuxiliar.setProximoNo(novoNo);
+    }
+
+    // privado - metodo interno da classe
+    private No<T> getNo(int indice) {
+
+        validaIndice(indice); // se passar indice q nao existe? nullpointerexception
+
+        int ultimoIndice = size() -1;
+        if (indice >= size()) {
+            throw new IndexOutOfBoundsException("Nao existe conteudo no indice " +indice + " desta lista. A lista só vai até o indice " + ultimoIndice);
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for (int i = 0; i < this.size() - 1; i++) {
+            noRetorno = noAuxiliar; // antes da passagem para proximo no, guarda o retorno
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+
+    }
+
+    // se passar um indice q nao existe? nullpointerexception
+    public void validaIndice(int indice) {
+        int ultimoIndice = size() -1;
+        if (indice >= size()) {
+            throw new IndexOutOfBoundsException("Nao existe conteudo no indice " +indice + " desta lista. A lista só vai até o indice " + ultimoIndice);
+        }
     }
 
     public boolean isEmpty() {
