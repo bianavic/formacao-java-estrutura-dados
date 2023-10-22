e0717ffe4cfb160ef77278ae5da904428e5aa038
bianavic <bianavic@gmail.com>
2023-10-22 12:44:50 -0300

    [lista circular] utilizacao metodos

--- src/main/java/org/edu/fabs/lista/circular/Main.java
+++ src/main/java/org/edu/fabs/lista/circular/Main.java
@@ -4,7 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
 
+        minhaListaCircular.add("c0"); // cabeca e cauda
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0); // vazio
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c1"); // cabeca e cauda
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c2"); // cauda
+        minhaListaCircular.add("c3"); // cauda
+        System.out.println(minhaListaCircular);
+
+        // MODO 1
+//        System.out.println(minhaListaCircular.get(0)); // cauda
+//        System.out.println(minhaListaCircular.get(1)); // meio
+//        System.out.println(minhaListaCircular.get(2)); // cabeca
+//
+//        System.out.println(minhaListaCircular.get(3)); // LOOP, ele retorna para indice 0
+//        System.out.println(minhaListaCircular.get(4)); // LOOP continua
+
+        // MODO 2
+        for (int i = 0; i < 20; i++) {
+            System.out.println(minhaListaCircular.get(i));
+        }
 
     }
 
