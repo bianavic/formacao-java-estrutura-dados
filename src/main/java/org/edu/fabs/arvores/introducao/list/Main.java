af8cfc93fb02389061577c447b273bfad911a733
bianavic <bianavic@gmail.com>
2023-10-23 10:08:48 -0300

    [arvores] list - metodos add, contains, get, indexof, remove

--- src/main/java/org/edu/fabs/arvores/introducao/list/Main.java
+++ src/main/java/org/edu/fabs/arvores/introducao/list/Main.java
@@ -9,19 +9,24 @@ public class Main {
 
         List<Carro> carroList = new ArrayList<>();
 
+        // ADD
         carroList.add(new Carro("Ford"));
         carroList.add(new Carro("Chevrolet"));
         carroList.add(new Carro("Fiat"));
         carroList.add(new Carro("Peugeot"));
 
+        // CONTAINS
         System.out.println(carroList.contains(new Carro("Chevrolet")));
 
+        // GET
         System.out.println(carroList.get(2));
 
+        // INDEXOF
         System.out.println(carroList.indexOf(new Carro("Fiat")));
 
         System.out.println(carroList);
 
+        // REMOVE
 //        System.out.println(carroList.remove(new Carro("Fiat")));
         System.out.println(carroList.remove(2));
         System.out.println(carroList);
